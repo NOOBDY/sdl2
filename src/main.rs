be558fed0843b5eb3e5bd1b5f52fe5933fe65a1b
noobdy <htommyf12@gmail.com>
2021-10-16 10:59:07 +0800

    filled the screen with a weird purple color

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+extern crate gl;
 extern crate sdl2;
 
 fn main() {
     let _sdl = sdl2::init().unwrap();
     let video_subsystem = _sdl.video().unwrap();
-    let _window = video_subsystem
+    let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.6, 0.3, 0.7, 1.0);
+    }
 
     let mut event_pump = _sdl.event_pump().unwrap();
     'main: loop {
@@ -17,5 +27,11 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
