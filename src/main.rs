378d4b49c477a5a185969c239c1b12104d76e7d6
noobdy <htommyf12@gmail.com>
2021-10-16 10:28:31 +0800

    added a empty sdl2 window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let _sdl = sdl2::init().unwrap();
+    let video_subsystem = _sdl.video().unwrap();
+    let _window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = _sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+    }
 }
