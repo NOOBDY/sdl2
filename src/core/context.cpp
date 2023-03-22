abbde8828838a091e63dd42860429ef996feae95
noobdy <htommyf12@gmail.com>
2023-03-22 17:03:15 +0800

    opengl version issue

--- src/core/context.cpp
+++ src/core/context.cpp
@@ -9,15 +9,15 @@
 
 namespace Core {
 Context::Context(std::string title, int posX, int posY, int width, int height) {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "Failed to initialize SDL\n";
         std::cout << SDL_GetError() << "\n";
     }
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-                        SDL_GL_CONTEXT_PROFILE_CORE);
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
     m_Window = SDL_CreateWindow(             //
         title.c_str(),                       // Title name
@@ -31,9 +31,9 @@ Context::Context(std::string title, int posX, int posY, int width, int height) {
     m_Context = SDL_GL_CreateContext(m_Window); // Currently unused
 
     if (!gladLoadGL(reinterpret_cast<GLADloadfunc>(SDL_GL_GetProcAddress))) {
-        GLuint err = glad_glGetError();
+        GLuint err = glGetError();
         std::cout << "Failed to initialize OpenGL\n";
-        std::cout << glad_glGetString(err);
+        std::cout << glGetString(err);
     }
 }
 
